536c9df92a44d6ebbfc35dba102f4134fde36fda
Nireekshaamin <nireekshaamin2000@gmail.com>
2022-09-13 17:43:56 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    CardsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
